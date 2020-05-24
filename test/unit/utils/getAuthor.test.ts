904d57ed411a9f0b2a008c3c2ae8ac275a30ff7a
Nathan Taylor <nathamtaylor49@gmail.com>
2020-05-24 19:57:29 +0300

    fix typo in test

--- test/unit/utils/getAuthor.test.ts
+++ test/unit/utils/getAuthor.test.ts
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 import {getAuthor} from '../../../src/utils/getAuthor';
-import { ICreatorDetails } from '../../../src/types';
+import { CreatorDetails } from '../../../src/types';
 
 describe('getAuthor', () => {
   describe('when creatorDetails is null', () => {
@@ -12,28 +12,28 @@ describe('getAuthor', () => {
 
   describe('when creatorDetails is first element is null', () => {
     it('returns null value', () => {
-      const result = getAuthor({} as ICreatorDetails);
+      const result = getAuthor({} as CreatorDetails);
       expect(result).to.eql(null);
     });
   });
 
   describe('when `pgterms:agent` is not found', () => {
     it('returns null value', () => {
-      const result = getAuthor([{}] as ICreatorDetails);
+      const result = getAuthor([{}] as CreatorDetails);
       expect(result).to.eql(null);
     });
   });
 
   describe('when `pgterms:agent` is found but first element is null', () => {
     it('returns null value', () => {
-      const result = getAuthor([{'pgterms:agent': {}}] as ICreatorDetails);
+      const result = getAuthor([{'pgterms:agent': {}}] as CreatorDetails);
       expect(result).to.eql(null);
     });
   });
 
   describe('when `pgterms:name` is found', () => {
     it('returns name value', () => {
-      const result = getAuthor([{'pgterms:agent': [{'pgterms:name': ['authorName']}]}] as ICreatorDetails);
+      const result = getAuthor([{'pgterms:agent': [{'pgterms:name': ['authorName']}]}] as CreatorDetails);
       expect(result).to.eql('authorName');
     });
   });
